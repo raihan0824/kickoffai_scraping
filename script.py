31c30482ca7f5c199ebbe0f2d335e6b479e807f0
raihan0824 <84271135+raihan0824@users.noreply.github.com>
2022-03-16 16:16:10 +0700

    Update script.py

--- script.py
+++ script.py
@@ -9,14 +9,19 @@ from selenium.webdriver.common.by import By
 import re
 import json
 import time
+import urllib.request, urllib.parse, urllib.error
+import re
+import string
+from urllib.request import urlopen
 
+i=0
 url = 'https://kickoff.ai/matches'
 options = Options()
 options.add_argument("user-data-dir=/Users/raihanafiandi/Library/Application Support/Google/Chrome/Default")
 driver = webdriver.Chrome(chrome_options=options)    # creating chrome instance
 driver.get(url)
-time.sleep(3)
-while True:
+time.sleep(5)
+while i<=10: # click the show more button 10 times, if you want to fetch all pages, change to while True
     try:
         show_more = driver.find_element_by_xpath("//button[text()='Show more']")
         driver.execute_script("arguments[0].click();", show_more)
@@ -24,9 +29,10 @@ while True:
     except:
         print('No more show more button')
         break
-time.sleep(3)
+    i+=1
+time.sleep(10)
 ss = driver.page_source
-soups=BeautifulSoup(ss,'html.parser') 
+soup=BeautifulSoup(ss,'html.parser').findAll('div',{"class":"col-hlf-ko"})[1] # only get past results data
 time.sleep(1)
 
 DATE=[]
@@ -41,19 +47,19 @@ HOME_KICK_SCORE=[]
 AWAY_KICK_SCORE=[]
 
 links=[]
-for soup in soups:
-    links.append('https://kickoff.ai'+soup.findAll('a')[0].get('href'))
+for i in soup.findAll('a',href = re.compile('match/.+')):
+    links.append('https://kickoff.ai'+i.get('href'))
  
 for link in links:
-    soup = BeautifulSoup(urlopen(link),'html.parser')
-    DATE.append(soup.find('div',{"class":"match-time"}).text.strip()) # DATE
-    HOME.append(soup.find('div',{"class":"team-home"}).text.strip()) # HOME
-    AWAY.append(soup.find('div',{"class":"team-away"}).text.strip()) # AWAY
-    HOME_GOAL.append(soup.find('div',{"class":"result"}).text.strip()[0]) # HOME_GOAL
-    AWAY_GOAL.append(soup.find('div',{"class":"result"}).text.strip()[4]) # AWAY_GOAL
-    HOME_WIN.append(soup.find('span',{"class":"prediction-win-home"}).text.strip()) # HOME_WIN
-    DRAW_WIN.append(soup.find('span',{"class":"prediction-draw text-muted"}).text.strip()) # DRAW_WIN
-    AWAY_WIN.append(soup.find('span',{"class":"prediction-win-away"}).text.strip()) # AWAY_WIN
+    soup2 = BeautifulSoup(urlopen(link),'html.parser')
+    DATE.append(soup2.find('div',{"class":"match-time"}).text.strip()) # DATE
+    HOME.append(soup2.find('div',{"class":"team-home"}).text.strip()) # HOME
+    AWAY.append(soup2.find('div',{"class":"team-away"}).text.strip()) # AWAY
+    HOME_GOAL.append(soup2.find('div',{"class":"result"}).text.strip()[0]) # HOME_GOAL
+    AWAY_GOAL.append(soup2.find('div',{"class":"result"}).text.strip()[4]) # AWAY_GOAL
+    HOME_WIN.append(soup2.find('span',{"class":"prediction-win-home"}).text.strip()) # HOME_WIN
+    DRAW_WIN.append(soup2.find('span',{"class":"prediction-draw"}).text.strip()) # DRAW_WIN
+    AWAY_WIN.append(soup2.find('span',{"class":"prediction-win-away"}).text.strip()) # AWAY_WIN
 
 pd.DataFrame(
     {
